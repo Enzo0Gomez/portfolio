64c48a34d1db6d0adb6bb587edf6b43cb50cd7d8
Dan Raizen Gomez <gomezraizendan@gmail.com>
2026-08-31 14:36:56 +0800

    edit details

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -9,8 +9,8 @@ import setupImage from '../assets/pictures/my_internship/setup.png';
 import hardwareImage from '../assets/pictures/my_internship/Hardware_basic.png';
 import officeImage from '../assets/pictures/my_internship/office.png';
 import practiceVlan from '../assets/pictures/my_internship/practice_create_vlan.png';
-import noc from '../assets/pictures/my_internship/noc.png';
-import system from '../assets/pictures/my_internship/system_deparment.png';
+import noc from '../assets/pictures/my_internship/NOC.png';
+import system from '../assets/pictures/my_internship/system_department.png';
 
 const galleryItems = [
     {
